8ced89f86db87007582bddd2e43edc129fec321c
mitalrs <mitalsapkale1999@gmail.com>
2023-04-27 23:30:32 +0530

    AppBAr added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import AppBar from './Component/AppBar';
 
 function App() {
   return (
     <div className="App">
+      <AppBar/>
       app
     </div>
   );
